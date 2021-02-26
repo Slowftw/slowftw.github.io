978f478c04cec783c95a2c104e02f3d68d4f6555
Slowftw <46169656+Slowftw@users.noreply.github.com>
2021-02-26 02:13:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express"),
 app = express();
+app.listen(process.env.PORT);
 app.get("/", (req, res) => {
   res.sendFile(__dirname+"/index.html");
 });
@@ -8,9 +9,6 @@ app.get("/hash", (req, res) => {
 });
 app.get("/test", (req, res) => {
   res.sendFile(__dirname+"/t.js");
-});
-const listener = app.listen(process.env.PORT, () => {
-  console.log(listener.address().port);
 });
 app.use(function (req, res, next) {
   res.status(404).sendFile(__dirname+"/404.html");
