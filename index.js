cbcbf1b541cb66a6f57db26ce4e661fdcc9775cf
Slowftw <46169656+Slowftw@users.noreply.github.com>
2021-02-26 01:44:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const path = require("path"),
 express = require("express"),
 app = express();
-app.use(express.static(path.join(__dirname, "res")));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, "res", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "res", "index.html"))
 });
 app.get("/hash", (req, res) => {
-  res.sendFile(path.join(__dirname, "res", "hash.html"));
+  res.sendFile(path.join(__dirname, "res", "hash.html"))
 });
 app.get("/test", (req, res) => {
-  res.sendFile(path.join(__dirname, "res", "t.js"));
+  res.sendFile(path.join(__dirname, "res", "t.js"))
 });
-
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
 const listener = app.listen(process.env.PORT, () => {
   console.log(listener.address().port);
 });
