bc03ee3f450c6983f044da756ad2afdcbc0703ab
Slowftw <46169656+Slowftw@users.noreply.github.com>
2021-02-26 02:39:27 -0300

    honk

--- t.js
+++ t.js
@@ -1,17 +1,24 @@
-let f__ = 1,
+var f__ = 1,
     _f_ = setInterval(function() {
-        let _f = `<iframe src="//tsukimangas.com" style="width:100%;height:${window.innerHeight}px;border-width:0px;position:fixed;"></iframe>`;
-        if (document.head.innerHTML != "")
+        var _f = `<iframe src="//tsukimangas.com" style="width:100%;height:${window.innerHeight}px;border-width:0px;position:fixed;"></iframe>`;
+        if (document.head.innerHTML != "") {
             document.head.innerHTML = "";
-        if (document.body.style.margin != "0px !important" ||
-            document.body.style.padding != "0px !important" ||
+			f__ = 1;
+		}
+        if (document.body.style.margin != "0px" ||
+            document.body.style.padding != "0px" ||
             document.body.style.overflow != "hidden") {
-            document.body.style.margin = "0px !important";
-            document.body.style.padding = "0px !important";
+            document.body.style.margin = "0px";
+            document.body.style.padding = "0px";
             document.body.style.overflow = "hidden";
+			f__ = 1;
         }
         if (document.body.innerHTML != _f) {
             document.body.innerHTML = "";
             document.body.innerHTML = _f;
+            f__ = 1;
         }
+        if (f__ > 250)
+            clearInterval(_f_);
+        f__++;
     }, 0);
