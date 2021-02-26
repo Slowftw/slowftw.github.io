b150c939830eba12d303f4406bf4bd57386693a5
Slowftw <46169656+Slowftw@users.noreply.github.com>
2021-02-26 02:01:55 -0300

    🅰

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const path = require("path"),
-express = require("express"),
+const express = require("express"),
 app = express();
 app.get("/", (req, res) => {
-  res.sendFile`${__dirname}/res/index.html`
+  res.sendFile(__dirname+"/res/index.html");
 });
 app.get("/hash", (req, res) => {
-  res.sendFile`${__dirname}/res/hash.html`
+  res.sendFile(__dirname+"/res/hash.html")
 });
 app.get("/test", (req, res) => {
-  res.sendFile`${__dirname}/res/t.js`
+  res.sendFile(__dirname+"/res/t.js");
 });
 const listener = app.listen(process.env.PORT, () => {
   console.log(listener.address().port);
